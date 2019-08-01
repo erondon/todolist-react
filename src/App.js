4b5d18eb00c1d0ad82afb99faccbe0c0a0b2e90c
Eleazar <erondon@gmail.com>
2019-07-31 21:33:55 -0400

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Fragment} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      tareas : [],
+      tarea :'',
+
+    }
+    
+  } 
+
+onChange = (event) => {
+  this.setState({term: event.target.value});
+
+
 }
 
+onSubmit = (event) => {
+  event.preventDefault()
+  this.setState({
+    term: '',
+    items: [...this.state.items, this.state.term]
+
+  });
+}
+
+
+
+
+
+  
+  render(){
+   return(
+    <div>
+
+
+<input value={this.state.term} onChange={this.onChange} />
+<form className="App" onSubmit={this.onSubmit}>
+       <input value={this.state.term} onChange={this.onChange} />
+       <button>Submit</button>
+   </form>
+
+    </div>
+     ); 
+  }
+
+}
 export default App;

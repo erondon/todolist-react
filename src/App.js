e14d41e1bae86e498166ddfbebbeb0d781102e7c
Eleazar <erondon@gmail.com>
2019-08-02 12:09:36 -0400

    funciona pero le falta el metodo de remover el ToDO

--- src/App.js
+++ src/App.js
@@ -1,54 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import {Fragment} from 'react';
+//import { directive } from '@babel/types';
+//import {Fragment} from 'react';
+//import { ninvoke } from 'q';
 
 class App extends React.Component {
-  constructor(){
-    super();
+
+  constructor(props){
+    super(props);
 
     this.state = {
-      tareas : [],
-      tarea :'',
-
+      userInput: '',
+      list: [],
+   
     }
-    
-  } 
-
-onChange = (event) => {
-  this.setState({term: event.target.value});
-
-
-}
-
-onSubmit = (event) => {
-  event.preventDefault()
+  }
+changeUserInput(input){
   this.setState({
-    term: '',
-    items: [...this.state.items, this.state.term]
-
+    userInput: input,
+    
   });
 }
+addToList(input){
+
+  let listArray = this.state.list;
+
+  listArray.push(input);
 
 
+  this.setState({
+    list: listArray,
+    userInput: ''
+  })
+}
 
-
-
+removeFromLIst(){
   
-  render(){
-   return(
-    <div>
+  this.setState({
+    
+  })
+}
+render(){
 
+  return (
+    <div className='container'>
+      <div className='row'>
+       <div className='col-md-12'>
+         <h1>React ToDo List</h1>
+       <input
+       className='form-control'
+      onChange={(e)=>this.changeUserInput(e.target.value)}
+      value={this.state.userInput}
+      type='text'
+      />
+      <button className='btn btn-success'onClick={()=>this.addToList(this.state.userInput)}>Add</button>
 
-<input value={this.state.term} onChange={this.onChange} />
-<form className="App" onSubmit={this.onSubmit}>
-       <input value={this.state.term} onChange={this.onChange} />
-       <button>Submit</button>
-   </form>
+      <ul> 
 
+      {this.state.list.map((val)=><h2>{val}</h2>)}
+
+      </ul>
+       </div>
+     
+      </div>
+      
     </div>
-     ); 
-  }
-
+  )
+}
 }
 export default App;
